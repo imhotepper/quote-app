8ca5ef8b8ddf775559eb80a92759eafccbe021bf
Dragos <dragosh@MacBook-Pro.local>
2017-02-20 20:34:57 +0200

    some logging aded

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+console.log("serve it from: ",path.join( __dirname , "/build/unbundled"));
+
 app.use(express.static(path.join( __dirname , "/build/unbundled")));
 app.listen(process.env.PORT || 5000);
