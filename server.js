08f279be971c0316ab2a5b52aba628d25cbf5f27
Dragos <dragosh@MacBook-Pro.local>
2017-02-20 20:40:34 +0200

    serving root index

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-console.log("serve it from: ",path.join( __dirname , "/build/unbundled"));
+console.log("serve it from: ",path.join( __dirname , "/"));
 
 app.use('/bower_components', express.static('./bower_components'))
 app.use(express.static(path.join( __dirname , "/build/unbundled")));
